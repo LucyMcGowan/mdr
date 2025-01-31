3507379c0918d624339eebd70e6abc2438f0df69
Lucy D'Agostino McGowan <lucydagostino@gmail.com>
2025-01-31 08:51:11 -0500

    update package

--- R/badges.R
+++ R/badges.R
@@ -1,2 +1,8 @@
 #' Badge data
+#'
+#' Data on the employee badges. The variable `joined` is the approximate days
+#' prior to Helly joining that the other employees joined Lumon. Mark is 730,
+#' (2 years), since in Season 1 Episode 1 (at 40:39) Mark says he joined about
+#' two years ago, Irving is 3,285 (9 years), since a marketing Linkedin post
+#' from Lumon stated he joined 9 years prior.
 "badges"
--- data-raw/badges.R
+++ data-raw/badges.R
@@ -9,36 +9,4 @@ badges <- badges |>
          joined = joined * 365) |>
   arrange(id_numeric)
 
-preds <- tibble(id_numeric = 8039:8988)
-preds$joined_predicted <- lm(joined ~ I(id_numeric^30), badges) |>
-  predict(newdata = preds)
-ggplot(badges, aes(x = id_numeric, y = joined_predicted)) +
-  geom_point() +
-  geom_line(data = preds, aes(y = joined_predicted), color = "white", linetype = "dashed") +  # Add the dashed line
-  scale_y_continuous(
-    breaks = badges$joined_predicted,
-    labels = badges$name[order(badges$joined_predicted, decreasing = TRUE)],
-    sec.axis = sec_axis(~ ., breaks = badges$joined_predicted, labels = round(badges$joined_predicted / 365, 1), name = "Predicted Start (Years Ago)")
-  ) +
-  scale_x_continuous(
-    breaks = badges$id_numeric,
-    labels = function(x) glue::glue("08-{substr(x, 2, 4)}")
-  ) +
-  labs(x = "Employee Code", y = "") +
-  annotate("label", x = 8907, y = 365*2,
-           size = 3,
-           label = "~2 years\nsource: S1E1 (40:39)",
-           hjust = 1) +
-  annotate("label", x = 8430, y = 365*9,
-           size = 3,
-           label = "~9 years\nsource: Linkedin",
-           hjust = 1) +
-  theme_mdr() +
-  theme(
-    panel.grid.minor = element_blank(),
-    axis.text.x = element_text(angle = 45, hjust = 1)
-  )
-
-usethis::use_data(badges, overwrite = TRUE)
-
 usethis::use_data(badges, overwrite = TRUE)
