11077d0971de9f387e179c1871d751ac31800042
Lucy D'Agostino McGowan <lucydagostino@gmail.com>
2025-01-30 21:06:14 -0500

    add badges

--- data-raw/badges.R
+++ data-raw/badges.R
@@ -1,27 +1,44 @@
 library(tidyverse)
 
 badges <- read_csv("data-raw/badges.csv")
-badges <- badges |>
-  mutate(id_numeric = as.numeric(gsub("-","", id)))
 
-ggplot(badges, aes(x = id_numeric, y = id_numeric)) +
+badges <- badges |>
+  mutate(id_numeric = as.numeric(gsub("-", "", id)),
+         joined_predicted = lm(joined ~ I(id_numeric^30), badges) |>
+           predict(newdata = badges) * 365,
+         joined = joined * 365) |>
+  arrange(id_numeric)
+
+preds <- tibble(id_numeric = 8039:8988)
+preds$joined_predicted <- lm(joined ~ I(id_numeric^30), badges) |>
+  predict(newdata = preds)
+ggplot(badges, aes(x = id_numeric, y = joined_predicted)) +
   geom_point() +
-  scale_y_continuous(breaks = badges$id_numeric,
-                     labels = badges$name) +
-  scale_x_continuous(breaks = badges$id_numeric,
-                     labels = function(x) glue::glue("08-{substr(x, 2, 4)}")) +
-  labs(x = "employee code", y = "") +
-  annotate("segment", x = 8880, xend = 8880,
-           y = 8927, yend = 8988, size = 1,
-           arrow = arrow(length = unit(0.01, "npc"), ends = "both"),
-           color = "green") +
-  annotate("text", x = 8880, y = 8999,
-           label = "~2 years", size = 5,
-           color = "green",
-           vjust = 0) +
+  geom_line(data = preds, aes(y = joined_predicted), color = "white", linetype = "dashed") +  # Add the dashed line
+  scale_y_continuous(
+    breaks = badges$joined_predicted,
+    labels = badges$name[order(badges$joined_predicted, decreasing = TRUE)],
+    sec.axis = sec_axis(~ ., breaks = badges$joined_predicted, labels = round(badges$joined_predicted / 365, 1), name = "Predicted Start (Years Ago)")
+  ) +
+  scale_x_continuous(
+    breaks = badges$id_numeric,
+    labels = function(x) glue::glue("08-{substr(x, 2, 4)}")
+  ) +
+  labs(x = "Employee Code", y = "") +
+  annotate("label", x = 8907, y = 365*2,
+           size = 3,
+           label = "~2 years\nsource: S1E1 (40:39)",
+           hjust = 1) +
+  annotate("label", x = 8430, y = 365*9,
+           size = 3,
+           label = "~9 years\nsource: Linkedin",
+           hjust = 1) +
   theme_mdr() +
   theme(
     panel.grid.minor = element_blank(),
-    axis.text.x = element_text(angle = 45, hjust = 1))
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  )
+
+usethis::use_data(badges, overwrite = TRUE)
 
 usethis::use_data(badges, overwrite = TRUE)
