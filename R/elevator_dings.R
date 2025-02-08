dea417334b06498b0844a77db138335682b73d05
Lucy D'Agostino McGowan <lucydagostino@gmail.com>
2025-02-08 10:29:40 -0500

    update

--- R/elevator_dings.R
+++ R/elevator_dings.R
@@ -1,15 +1,6 @@
 #' Elevator Ding Data
-#' @format ## `elevator_dings`
-#' A data frame with 48 rows and 8 columns:
-#' \describe{
-#'   \item{season}{Season}
-#'   \item{episode}{Episode number}
-#'   \item{time}{Time}
-#'   \item{character}{Character}
-#'   \item{pitch}{Pitch of elevator ding}
-#'   \item{io}{Innie or Outie?}
-#'   \item{action}{Action, one of: enters, sleeps, wakes up}
-#'   \item{note}{Notes}
-#' }
+#'
+#' The first part of this data set was based on data compiled by Reddit user
+#' Sam_Badi, the remainder was added by the package author.
 #' @source https://www.reddit.com/r/SeveranceAppleTVPlus/comments/w8imx3/a_complete_analysis_of_all_of_the_ding_sounds/
 "elevator_dings"
