b426c11303f1abc652880bf8b7afe7dfe7df9d1c
Lucy D'Agostino McGowan <lucydagostino@gmail.com>
2025-02-08 10:27:43 -0500

    add elevator ding data

--- data-raw/elevator-dings.R
+++ data-raw/elevator-dings.R
@@ -1,5 +1,10 @@
 library(tidyverse)
 elevator_dings <- read_csv("data-raw/elevator-dings.csv")
+elevator_dings |>
+  mutate(action = case_when(
+    action %in% c("enters", "exits") ~ "elevator opens",
+    .default = action
+  ))
 usethis::use_data(elevator_dings, overwrite = TRUE)
 
 
